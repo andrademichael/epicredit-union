a2eac84b0588a38ccca3fd73b38b5013ca62364c
Erica Nafziger and Michael Andrade <michael@>
2016-11-01 14:01:20 -0700

    user can register account and withdraw and deposit to the account

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,46 @@
 //business logic
 
+var Account = function(name, balance) {
+  this.name = name;
+  this.balance = balance;
+  this.history = [balance];
+};
 
+Account.prototype.withdraw = function(amount) {
+  this.balance = this.balance - amount;
+  this.history.unshift(this.balance);
+};
+
+Account.prototype.deposit = function(amount) {
+  this.balance = this.balance + amount;
+  this.history.unshift(this.balance);
+};
 
 
 //UI logic
 $(document).ready(function(){
+  $("form#registrationForm").submit(function(event){
+    event.preventDefault();
+    var nameInput = $("input#name").val();
+    var initialDepositInput = parseInt($("input#initialDeposit").val());
+    var userAccount = new Account(nameInput, initialDepositInput);
+    console.log(userAccount);
 
 
-});
+    $(".btn#transact").click(function(){
+      var withdrawalInput = parseInt($("input#withdrawal").val());
+      var depositInput = parseInt($("input#deposit").val());
+      var transactionTypeInput = $("input:radio[name=transactionType]:checked").val();
+      var amountInput = parseInt($("input#transactionAmount").val());
+
+      if (transactionTypeInput === "withdrawal") {
+        userAccount.withdraw(amountInput);
+      } else {
+        userAccount.deposit(amountInput);
+      }
+      console.log(userAccount);
+
+    });//end of click function
+
+  });//end of submit function
+});//end of document ready
